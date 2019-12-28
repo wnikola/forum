e0d74a551329b91d8fc11992d6bb13a9705da090
Nikola M <mihajlovicn44@gmail.com>
2019-12-28 16:11:39 +0100

    Implement signin redirect and logout, work on the navbar

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { signIn } from '../utilities/forum-service';
 
-const SignIn = ({ setUser, history }) => {
+const SignIn = ({ match, setUser, history }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -11,13 +11,17 @@ const SignIn = ({ setUser, history }) => {
         if (data.success) {
           setUser(data.user);
           // console.log(data);
-          history.push('/topics');
+          if (match.params.topic_id !== undefined) {
+            history.push(`/topic/${match.params.topic_id}`);
+          } else {
+            history.push('/topics');
+          }
         }
         // data.success ? history.push('/topics') : console.log(data, 'Nije ulogovan');
         // data.success ? console.log(data, 'Ulogovan') : console.log(data, 'Nije ulogovan');
       });
   }
-
+  console.log(match.params.topic_id)
   return (
     <div className="container">
       <form>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = ({ loggedIn }) => {
+const Header = ({ loggedIn, setUser }) => {
 
   if (loggedIn) {
     return (
@@ -10,8 +10,13 @@ const Header = ({ loggedIn }) => {
           <Link className='logo navbar-brand' to='/'>HashCode</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className='profile-title nav-link' to={`/profile/${loggedIn.user_id}`}>{loggedIn.username}</Link>
+              <li className="nav-item dropdown">
+                <span className="clickable nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{loggedIn.username}</span>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Link className='profile-title dropdown-item' to={`/profile/${loggedIn.user_id}`}>Profile</Link>
+                  <div className="dropdown-divider"></div>
+                  <span className="clickable dropdown-item" onClick={() => setUser()} >Log Out</span>
+                </div>
               </li>
             </ul>
           </div>
